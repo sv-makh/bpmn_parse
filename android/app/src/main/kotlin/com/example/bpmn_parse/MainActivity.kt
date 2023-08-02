3736072d9bcbdc31647ffefd9d5db67ff9508983
sv-makh <mithriam@mail.ru>
2023-08-02 14:04:38 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/bpmn_parse/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bpmn_parse
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/bpmn_element.dart
@@ -0,0 +1,11 @@
+class BpmnElement {
+  String id;
+  String type;
+  String name;
+  String condition;
+  String subprocessId;
+  String description;
+  List<dynamic> inVariables;
+  String outVariables;
+
+}
